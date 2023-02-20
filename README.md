3cb30ec4e5fa803b51970463a447697a76bfc92a
sor4chi <sorakawa0218@gmail.com>
2023-02-21 04:49:36 +0900

    feat(dbmate): usersテーブルのmigrationを記述

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 1. `dbmate new create_users_table` でマイグレーションファイルを作成
    1. `db/migrations/[timestamp]_create_users_table.sql` が作成される
+2. `db/migrations/[timestamp]_create_users_table.sql` にSQLを記述
+   1. `-- migrate:up` にはマイグレーション作成のSQLを記述
+   2. ｀-- migrate:down` にはRollback用のSQLを記述
--- db/migrations/20230220194446_create_users_table.sql
+++ db/migrations/20230220194446_create_users_table.sql
@@ -1,5 +1,13 @@
 -- migrate:up
-
+create table users (
+  id serial primary key,
+  name varchar(255) not null,
+  email varchar(255) not null,
+  password varchar(255) not null,
+  created_at timestamp not null default now(),
+  updated_at timestamp not null default now()
+);
 
 -- migrate:down
+drop table users;
 
